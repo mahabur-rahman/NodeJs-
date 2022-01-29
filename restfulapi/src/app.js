e6a19926fd2345da6b9e244ab6625497f009c284
mahabur  rahman <annur4395@gmail.com>
2022-01-29 13:23:00 +0600

    restfull api

---
+++ restfulapi/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const Student = require("./models/students");
+// db
+require("./db/conn");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello world my friend</h1>");
+// });
+
+// middleware
+app.use(express.json());
+
+// create a new students
+app.post("/students", (req, res) => {
+  console.log(req.body);
+  const user = new Student(req.body);
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+  //   res.send("Hello Post method ");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
